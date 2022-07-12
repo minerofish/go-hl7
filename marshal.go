c2fc54f55d11810d0ad93d5f609e89f5eeeac2ff
Dávid Hatalák <HDave4488@gmail.com>
2022-07-12 10:25:20 +0200

    Do not add extra field delimiter at the end of processing

--- marshal.go
+++ marshal.go
@@ -34,28 +34,20 @@ type OutputRecord struct {
 
 type OutputRecords []OutputRecord
 
-func processStruct(message interface{}, depth int, enc Encoding, location *time.Location, notation Notation,
-	delimiters Delimiters) ([][]byte, error) {
-
+func processStruct(message interface{}, depth int, enc Encoding, location *time.Location, notation Notation, delimiters Delimiters) ([][]byte, error) {
 	buffer := make([][]byte, 0)
-
 	messageValue := reflect.ValueOf(message)
 	messageType := reflect.TypeOf(message)
 
 	for i := 0; i < messageValue.NumField(); i++ {
-
 		currentRecord := messageValue.Field(i)
 		currentRecordTag := messageType.Field(i).Tag.Get("hl7")
 		currentRecordTagsList := strings.Split(currentRecordTag, ",")
 
 		if len(currentRecordTag) == 0 { // no annotation = Descend if its an array or a struct of such
-
 			if currentRecord.Kind() == reflect.Slice { // array of something = iterate and recurse
-
 				for x := 0; x < currentRecord.Len(); x++ {
-
 					dood := currentRecord.Index(x).Interface()
-
 					if bytes, err := processStruct(dood, depth+1, enc, location, notation, delimiters); err != nil {
 						return nil, err
 					} else {
@@ -66,7 +58,6 @@ func processStruct(message interface{}, depth int, enc Encoding, location *time.
 				}
 
 			} else if currentRecord.Kind() == reflect.Struct { // got the struct straignt = recurse directly
-
 				if bytes, err := processStruct(currentRecord.Interface(), depth+1, enc, location, notation, delimiters); err != nil {
 					return nil, err
 				} else {
@@ -74,15 +65,10 @@ func processStruct(message interface{}, depth int, enc Encoding, location *time.
 						buffer = append(buffer, bytes[line])
 					}
 				}
-
 			} else {
-
 				return nil, fmt.Errorf("invalid Datatype without any annotation '%s'. You can use struct or slices of structs", currentRecord.Kind())
-
 			}
-
 		} else { // an annotated Record
-
 			recordType := currentRecordTagsList[0]
 
 			switch currentRecord.Kind() {
@@ -109,20 +95,16 @@ func processStruct(message interface{}, depth int, enc Encoding, location *time.
 					currentRecord.Type().Name(), currentRecordTag)
 			}
 		}
-
 	}
 
 	return buffer, nil
 }
 
 func processSegment(recordType string, subDepth int, currentRecord reflect.Value, generatedSequenceNumber int, location *time.Location, delimiters Delimiters) (string, error) {
-
 	fieldList := make(OutputRecords, 0)
-
 	isWorthGeneratingThisRecord := false
 
 	for i := 0; i < currentRecord.NumField(); i++ {
-
 		field := currentRecord.Field(i)
 		fieldTag := currentRecord.Type().Field(i).Tag.Get("hl7")
 		fieldTagsList := strings.Split(fieldTag, ",")
@@ -160,7 +142,6 @@ func processSegment(recordType string, subDepth int, currentRecord reflect.Value
 				value = fmt.Sprintf("%d", generatedSequenceNumber)
 				generatedSequenceNumber = generatedSequenceNumber + 1
 			}
-
 			fieldList = addFieldToOutput(fieldList, fieldIdx, repeatIdx, componentIdx, value)
 		case reflect.Float32, reflect.Float64:
 			//TODO: Add annotation for amount of decimals
@@ -188,12 +169,9 @@ func processSegment(recordType string, subDepth int, currentRecord reflect.Value
 				}
 				value = value + oneElementStr
 			}
-
 			fieldList = addFieldToOutput(fieldList, fieldIdx, repeatIdx, componentIdx, value)
 		case reflect.Struct:
-
 			if field.Type().Name() == "Time" { // ToDo: Ambigious Time (time.Time or sthelse.Time ?)
-
 				time := field.Interface().(time.Time)
 				if !time.IsZero() {
 					isWorthGeneratingThisRecord = true
@@ -205,9 +183,9 @@ func processSegment(recordType string, subDepth int, currentRecord reflect.Value
 						fieldList = addFieldToOutput(fieldList, fieldIdx, repeatIdx, componentIdx, value)
 					}
 				}
-
 			} else { // structure
 				//TODO:Limit depth to go
+				// TODO: Add /r support
 				subbie, _ := processSegment("", subDepth+1, field, 0, location, delimiters)
 				if subbie != "" {
 					fieldList = addFieldToOutput(fieldList, fieldIdx, repeatIdx, componentIdx, subbie)
@@ -218,7 +196,6 @@ func processSegment(recordType string, subDepth int, currentRecord reflect.Value
 		default:
 			return "", fmt.Errorf("invalid field type '%s' in struct '%s', input not processed", field.Type().Name(), currentRecord.Type().Name())
 		}
-
 	}
 
 	generatePreceedingAndTrailingDelimiters := true
@@ -238,6 +215,7 @@ func processSegment(recordType string, subDepth int, currentRecord reflect.Value
 		thisdelimiter = delimiters.SubSub
 		thisdelimiter = ""
 	}
+
 	str, hasData := generateHL7String(recordType, fieldList, delimiters,
 		generatePreceedingAndTrailingDelimiters, thisdelimiter, nextdelimiter)
 	// fmt.Printf("Build string '%s' (%d)\n", str, subDepth)
@@ -256,7 +234,6 @@ func processSegment(recordType string, subDepth int, currentRecord reflect.Value
 }
 
 func addFieldToOutput(data []OutputRecord, field, repeat, component int, value string) []OutputRecord {
-
 	or := OutputRecord{
 		Field:     field,
 		Component: component,
@@ -292,33 +269,23 @@ func (or OutputRecords) Swap(i, j int) { or[i], or[j] = or[j], or[i] }
 //	-> .... "|first-arr1^^third-arr1\fist-arr2^second-arr2|"
 //
 //	returns the line as string for output to file
-func generateHL7String(recordtype string, fieldList OutputRecords, delimiters Delimiters,
-	generatePreceedingAndTrailingDelimiters bool, THISDELIMTER, NEXTDELIMITER string) (string, bool) {
-
-	output := ""
-
+func generateHL7String(recordtype string, fieldList OutputRecords, delimiters Delimiters, generatePreceedingAndTrailingDelimiters bool, THISDELIMTER, NEXTDELIMITER string) (string, bool) {
 	sort.Sort(fieldList)
 
+	output := ""
 	componentbuffer := make([]string, 100)
 	maxComponent := 0
-
 	maxRepeat := 0
-
-	// add a terminator to reduce abortion--spaghetti-code
-	fieldList = append(fieldList, OutputRecord{Field: -1})
-
-	fieldGroup := -1 // groupchange on every field-change
-
+	fieldList = append(fieldList, OutputRecord{Field: -1}) // add a terminator to reduce abortion--spaghetti-code
+	fieldGroup := -1                                       // groupchange on every field-change
 	hasProducedAnyOutput := false
+	lastGeneratedFieldNo := -1 // this will help to fill in missing fields
 
 	if generatePreceedingAndTrailingDelimiters {
 		output = output + recordtype + THISDELIMTER // Record-ID, typical "H", "R", "O", .....
 	}
 
-	lastGeneratedFieldNo := -1 // this will help to fill in missing fields
-
 	for _, field := range fieldList {
-
 		// fill up fields just in case we skipped a field in defnition
 		if lastGeneratedFieldNo > 0 && lastGeneratedFieldNo < field.Field {
 			for i := 0; i < field.Field-lastGeneratedFieldNo; i++ {
@@ -328,9 +295,7 @@ func generateHL7String(recordtype string, fieldList OutputRecords, delimiters De
 		}
 
 		fieldGroupBreak := field.Field != fieldGroup && fieldGroup != -1
-
 		if fieldGroupBreak {
-
 			buffer := ""
 			for c := 0; c <= maxComponent; c++ {
 				buffer = buffer + componentbuffer[c]
@@ -342,15 +307,6 @@ func generateHL7String(recordtype string, fieldList OutputRecords, delimiters De
 			if buffer != "" {
 				output = output + buffer
 				hasProducedAnyOutput = true
-			}
-
-			if generatePreceedingAndTrailingDelimiters {
-				// output = output + ">" + THISDELIMTER
-			} else {
-				//place delimiters everywhere, except lastf
-				//if fieldIndex < len(fieldList)-1 {
-				output = output + THISDELIMTER
-				//}
 			}
 
 			maxRepeat = 0
@@ -371,6 +327,7 @@ func generateHL7String(recordtype string, fieldList OutputRecords, delimiters De
 		if field.Component > maxComponent {
 			maxComponent = field.Component
 		}
+
 		if field.Repeat > maxRepeat {
 			maxRepeat = field.Repeat
 		}
